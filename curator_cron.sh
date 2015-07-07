ef4bdc7aa274404110c1e899cb6f1a3e43e72b90
Robert Vulpe <nytr0gen@users.noreply.github.com>
2015-07-07 15:35:13 +0300

    fixed delete disk space

--- curator_cron.sh
+++ curator_cron.sh
@@ -7,7 +7,7 @@ fi
 
 while true; do
     /usr/local/bin/curator delete indices --prefix logstash --older-than 14 --time-unit days --timestring "%Y.%m.%d"
-    /usr/local/bin/curator delete indices --prefix logstash --disk-space 8 --time-unit days --timestring "%Y.%m.%d"
+    /usr/local/bin/curator delete --disk-space 8 indices --prefix logstash --time-unit days --timestring "%Y.%m.%d"
     /usr/local/bin/curator bloom indices --prefix logstash --older-than 7 --time-unit days --timestring "%Y.%m.%d"
     /usr/local/bin/curator optimize indices --prefix logstash --older-than 1 --time-unit days --timestring "%Y.%m.%d"
     /usr/local/bin/curator allocation --rule tag=done_indexing indices --prefix logstash --older-than 1 --time-unit days --timestring "%Y.%m.%d"
